ef365c0119ec5e36c95108ddf104febc2fe4dd4e
Pelle <Pelle@LAPTOP-T69PBCOE>
2016-10-03 08:41:02 +0200

    Add project files.

---
+++ 11_Files/StockIO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Files
+{
+    class StockIO
+    {
+        internal void WriteStock(StringWriter sw, Stock hp)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
